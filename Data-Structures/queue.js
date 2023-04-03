0ffd42e87a68e0cfddf09a5297107f2dc3fdc794
H4K3R13 <arjunmundamani@gmail.com>
2023-04-03 20:58:20 +0530

    Add Queue 🚞

---
+++ Data-Structures/queue.js
@@ -0,0 +1,41 @@
+class Queue {
+
+    constructor(){
+        this.items = [];
+    }
+
+    enqueue(item){
+        this.items.push(item);
+    }
+
+    dequeue(item){
+        return this.items.shift();
+    }
+
+    peek(item){
+        return this.items[0];
+    }
+
+    isEmpty(){
+        return this.items.length === 0;
+    }
+
+}
+
+const queue = new Queue;
+
+queue.enqueue('A');
+queue.enqueue('R');
+queue.enqueue('J');
+
+console.log(queue.isEmpty());
+
+console.log(queue);
+
+console.log(queue.peek());
+
+queue.dequeue();
+console.log(queue);
+
+queue.enqueue('U');
+console.log(queue);
