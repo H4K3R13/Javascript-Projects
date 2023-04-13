cf068df74f98404ce24d038baa063b4e6c2d1571
H4K3R13 <arjunmundamani@gmail.com>
2023-04-13 21:06:36 +0530

    Wheather App 🌤️

---
+++ WheatherApp/script.js
@@ -0,0 +1,29 @@
+var inputval = document.querySelector("#cityinput")
+var btn = document.querySelector("#add")
+var city = document.querySelector("#cityouput")
+var desc = document.querySelector("#description")
+var temp = document.querySelector("#temp")
+var wind = document.querySelector("#wind")
+
+apik="3045dd712ffe6e702e3245525ac7fa38"
+
+function convertion(val){
+    return (val-273).toFixed(2)
+}
+
+btn.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid'+apik)
+    .then(res => console.log(res))
+    .then(data => {
+        var nameval = data['name']
+        var descrip = data['weather']['0']['description']
+        var temperature = data['main']['temp']
+        var wndspd = data['wind']['speed']
+
+        city.innerHTML=`Wheather of <span>${nameval}<span>`
+        city.innerHTML=`Temperature <span>${convertion(temperature)} C<span>`
+        description.innerHTML=`Sky Conditon <span>${descrip}<span>`
+        wind.innerHTML=`Wind <span>${wndspd}<span>`
+    })
+    .catch(err => alert('You Entered Wrong City Name'))
+})
